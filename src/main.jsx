940aec287f4cc08378f6ef9d5d67c4711990fa38
Sujal Saroj <sujalsaroj2006@example.com>
2025-07-30 21:37:09 +0530

    changes done

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/turf_booking">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
