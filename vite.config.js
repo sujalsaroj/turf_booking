9ca426f39be5d229f761d3cd1e24e5d21ef83b69
Sujal Saroj <sujalsaroj2006@example.com>
2025-07-30 21:29:08 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/turf_booking/", // 👈 set the correct sub-path
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
