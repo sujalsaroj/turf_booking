ad1ae7eafbe3364b26a75b8f7d20f1594f94dd8f
Sujal Saroj <sujalsaroj2006@example.com>
2025-07-30 20:27:53 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes/Approutes";
+// Remove this line: import { useLocation } from "react-router-dom";
 
 function App() {
+  // Remove this line: const location = useLocation();
+
   return (
     <>
       <Navbar />
--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,6 +1,5 @@
-// src/routes/AppRoutes.jsx
+// src/routes/AppRoutes.jsx - Change path="/" to index
 import { Routes, Route } from "react-router-dom";
-// import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import DashboardPlayer from "../pages/DashboardPlayer";
@@ -19,23 +18,26 @@ import Contact from "../pages/contactus";
 import Homepage from "../pages/Home";
 
 const AppRoutes = () => {
+  console.log("AppRoutes component is rendering");
   return (
     <Routes>
+      {/* Change this line from path="/" to index */}
       <Route path="/" element={<Homepage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard-player" element={<DashboardPlayer />} />
-      <Route path="/raise-connection" element={<RaiseConnection />} />
-      <Route path="/find-matches" element={<FindMatches />} />
-      <Route path="/join-match" element={<JoinConnection />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/dashboard-turf-owner" element={<DashboardTurfOwner />} />
-      <Route path="/addturf" element={<AddTurf />} />
-      <Route path="/manageturf" element={<ManageTurf />} />
-      <Route path="/bookTurf" element={<BookTurf />} />
-      <Route path="/mybooking" element={<ViewBookings />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact-us" element={<Contact />} />
+      <Route path="/home" element={<Homepage />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="dashboard-player" element={<DashboardPlayer />} />
+      <Route path="raise-connection" element={<RaiseConnection />} />
+      <Route path="find-matches" element={<FindMatches />} />
+      <Route path="join-match" element={<JoinConnection />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="dashboard-turf-owner" element={<DashboardTurfOwner />} />
+      <Route path="addturf" element={<AddTurf />} />
+      <Route path="manageturf" element={<ManageTurf />} />
+      <Route path="bookTurf" element={<BookTurf />} />
+      <Route path="mybooking" element={<ViewBookings />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact-us" element={<Contact />} />
     </Routes>
   );
 };
